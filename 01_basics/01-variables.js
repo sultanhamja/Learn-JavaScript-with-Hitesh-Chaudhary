ab344ac32883ed528d0bc11ad3c6f7aa29134bec
Hamja Sultan <sultanhamja597@gmail.com>
2025-06-11 07:51:52 +0000

    Variable in JS

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,9 @@
+let accountId = 786 // you can re define value of let variable 
+
+const accountEmail = "example@gmail.com" // can not change value of cosnt variable  
+
+var accountPassword = "1233456"  // do not use depriciated because 
+
+accountCity = "Sanjan"
+
+console.log(accountId);
