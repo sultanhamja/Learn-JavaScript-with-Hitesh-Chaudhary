cd787a8df7dbb545964c854067a182e4146e1983
Hamja Sultan <sultanhamja597@gmail.com>
2025-06-12 15:03:26 +0000

    datatypes in JavaScript

---
+++ 01_basics/data-types.js
@@ -0,0 +1,29 @@
+// TWO TYPES 
+
+// 1 PRIMITIVE
+    // 7 TYPES IN PRIMITIVE
+    // 1. String : "", ''
+    const name = "Hamja"
+    // 2. Numbers 
+    const age = 27
+    // 3. Boolean : true, false
+    const married = false
+    // 4. null : is a value on its own
+    const outSideTemp = null
+    // 5. undefined : value which is not defined
+    let girlFriend ; // you can also give the value undefined 
+    // 6. Symbol
+    const passion = Symbol('Coding') // it saves the variable reference in memory and uses it when called
+    const profession = Symbol('Coding') // so even though it looks same value as above, when you compare them its not.
+    // 7. BigInt : also a number but with big values 
+    const creditLimit = 9876543n // using n in the end represents that this is a bigInt 
+
+// NON-PREMITIVE (Reference type)
+    // 1. array : []
+    // 2. objects: {}
+    // 3. functions 
+
+
+
+
+    // NOTE: JavaScript is a Dynamic language 
